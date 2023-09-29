dfd210207df49d645a2e0b9230ed8c60cd87e928
Ali-Osman97 <ali.elmagnifigo@gmail.com>
2023-09-29 09:43:41 -0700

    aaa

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,17 +2,17 @@
 
 /**
  * set_bit - sets a bit at a given index to 1
- * @n: pointer to the number to change
+ * @x: pointer to the number to change
  * @index: index of the bit to set to 1
  *
  * Return: 1 for success, -1 for failure
  */
-int set_bit(unsigned long int *num, unsigned int index)
+int set_bit(unsigned long int *x, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
 
-	*num = ((1UL << index) | *num);
+	*x = ((1UL << index) | *x);
 	return (1);
 }
 
